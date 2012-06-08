789f69ed0463251c8158c28278ce65ccda276910
kaiwinter@gmail.com <kaiwinter@gmail.com@55b1c08c-e718-be89-1c06-3ab9208d3a18>
2012-06-08 08:58:28 +0000

    The statement now can handle the call of stored procedure. A CSV file with the name of the procedure is tried to load in this case.

--- DummyJdbc/src/com/google/dummyjdbc/statement/impl/CsvStatement.java
+++ DummyJdbc/src/com/google/dummyjdbc/statement/impl/CsvStatement.java
@@ -24,9 +24,9 @@ import com.google.dummyjdbc.resultset.impl.CSVResultSet;
 import com.google.dummyjdbc.statement.StatementAdapter;
 
 /**
- * This class does the actual work of the Generic... classes. It tries to open a CSV file for the table name in the
- * query and parses the contained data.
- *
+ * This class does the actual work of the Generic... classes. It tries to open a
+ * CSV file for the table name in the query and parses the contained data.
+ * 
  * @author Kai Winter
  */
 public final class CsvStatement extends StatementAdapter {
@@ -34,11 +34,17 @@ public final class CsvStatement extends StatementAdapter {
 	/** Pattern to get table name from an SQL statement. */
 	private static final Pattern TABLENAME_PATTERN = Pattern.compile(".*from (\\S*)\\s?.*", Pattern.CASE_INSENSITIVE);
 
+	/** Pattern to get the name of a stored procedure from an SQL statement. */
+	private static final Pattern STORED_PROCEDURE_PATTERN = Pattern.compile(".*(EXEC|EXECUTE) (\\S*)\\s?.*",
+			Pattern.CASE_INSENSITIVE);
+
 	private final Map<String, File> tableResources;
 
 	/**
 	 * Constructs a new {@link CsvStatement}.
-	 * @param tableResources {@link Map} of table name to CSV file.
+	 * 
+	 * @param tableResources
+	 *            {@link Map} of table name to CSV file.
 	 */
 	public CsvStatement(Map<String, File> tableResources) {
 		this.tableResources = tableResources;
@@ -47,42 +53,54 @@ public final class CsvStatement extends StatementAdapter {
 	@Override
 	public ResultSet executeQuery(String sql) throws SQLException {
 
-		Matcher matcher = TABLENAME_PATTERN.matcher(sql);
-		if (matcher.matches()) {
-			String tableName = matcher.group(1);
+		// Try to interpret SQL as a SELECT on a table
+		Matcher tableMatcher = TABLENAME_PATTERN.matcher(sql);
+		if (tableMatcher.matches()) {
+			String tableName = tableMatcher.group(1);
+			return createResultSet(tableName);
+		}
 
-			// Does a text file for the dummy table exist?
-			File ressource = tableResources.get(tableName.toLowerCase());
-			if (ressource == null) {
-				// Try to load a file from the ./tables/ directory
-				CodeSource src = CsvStatement.class.getProtectionDomain().getCodeSource();
+		// Try to interpret SQL as call of a stored procedure
+		Matcher storedProcedureMatcher = STORED_PROCEDURE_PATTERN.matcher(sql);
+		if (storedProcedureMatcher.matches()) {
+			String storedProcedureName = storedProcedureMatcher.group(2);
+			return createResultSet(storedProcedureName);
+		}
 
-				String path = src.getLocation().getPath();
-				path = path.substring(0, path.lastIndexOf("/"));
-				ressource = new File(new File(path), "/tables/" + tableName.toLowerCase() + ".csv");
-			}
+		return new DummyResultSet();
+	}
 
-			FileInputStream dummyTableDataStream = null;
-			try {
-				dummyTableDataStream = new FileInputStream(ressource);
-				return createGenericResultSet(tableName, dummyTableDataStream);
-			} catch (FileNotFoundException e) {
-				String message = MessageFormat.format("No table definition found for ''{0}'', using DummyResultSet.",
-						tableName);
-				System.err.println(message);
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} finally {
-				if (dummyTableDataStream != null) {
-					try {
-						dummyTableDataStream.close();
-					} catch (IOException e) {
-						// ignore
-						e.printStackTrace();
-					}
+	private ResultSet createResultSet(String tableName) {
+		// Does a text file for the dummy table exist?
+		File ressource = tableResources.get(tableName.toLowerCase());
+		if (ressource == null) {
+			// Try to load a file from the ./tables/ directory
+			CodeSource src = CsvStatement.class.getProtectionDomain().getCodeSource();
+
+			String path = src.getLocation().getPath();
+			path = path.substring(0, path.lastIndexOf("/"));
+			ressource = new File(new File(path), "/tables/" + tableName.toLowerCase() + ".csv");
+		}
+
+		FileInputStream dummyTableDataStream = null;
+		try {
+			dummyTableDataStream = new FileInputStream(ressource);
+			return createGenericResultSet(tableName, dummyTableDataStream);
+		} catch (FileNotFoundException e) {
+			String message = MessageFormat.format("No table definition found for ''{0}'', using DummyResultSet.",
+					tableName);
+			System.err.println(message);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} finally {
+			if (dummyTableDataStream != null) {
+				try {
+					dummyTableDataStream.close();
+				} catch (IOException e) {
+					// ignore
+					e.printStackTrace();
 				}
 			}
-
 		}
 
 		return new DummyResultSet();
