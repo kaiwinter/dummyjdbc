d17d2e08e9bc9fe2a289a23463086166855099f2
Massimiliano Siddi <msid@d-velop.de>
2021-08-15 00:05:55 +0200

    Tabbing for spaces

--- src/main/java/com/googlecode/dummyjdbc/DummyJdbcDriver.java
+++ src/main/java/com/googlecode/dummyjdbc/DummyJdbcDriver.java
@@ -7,7 +7,12 @@ import com.googlecode.dummyjdbc.utils.StringUtils;
 import java.io.File;
 import java.io.FileFilter;
 import java.net.URL;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.DriverPropertyInfo;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,146 +26,142 @@ import java.util.logging.Logger;
  */
 public final class DummyJdbcDriver implements Driver {
 
-    private final static String DEFAULT_DATABASE = "any";
+	private final static String DEFAULT_DATABASE = "any";
 
-    private static final Map<String, Map<String, File>> tableResources = Collections.synchronizedMap(
-            new HashMap<String, Map<String, File>>());
+	private static final Map<String, Map<String, File>> tableResources = Collections.synchronizedMap(
+		new HashMap<String, Map<String, File>>() );
 
-    static {
-        try {
-            // Register this with the DriverManager
-            DriverManager.registerDriver(new DummyJdbcDriver());
-        } catch (SQLException e) {
-            // ignore
-        }
-    }
+	static {
+		try {
+			// Register this with the DriverManager
+			DriverManager.registerDriver( new DummyJdbcDriver() );
+		} catch( SQLException e ) {
+			// ignore
+		}
+	}
 
-    /**
-     * Registers a CSV file for a database table. When a Query is executed like <code>SELECT * FROM ADDRESSES</code> the
-     * given <code>csvFile</code> for the given <code>tablename</code> <code>addresses</code> will be used.
-     *
-     * @param tablename The name of the database table like in the SQL statement (e.g. addresses).
-     * @param csvFile   A {@link File} object of a CSV file which should be parsed in order to return table data.
-     */
-    public static void addTableResource(String tablename, File csvFile) {
-        Map<String, File> databaseMap;
-        if (tableResources.containsKey(DEFAULT_DATABASE)) {
-            databaseMap = tableResources.get(DEFAULT_DATABASE);
-        } else {
-            databaseMap = Collections.synchronizedMap(new HashMap<String, File>());
-        }
-        databaseMap.put(tablename.toLowerCase(), csvFile);
-        tableResources.put(DEFAULT_DATABASE, databaseMap);
-    }
+	/**
+	 * Registers a CSV file for a database table. When a Query is executed like <code>SELECT * FROM ADDRESSES</code> the
+	 * given <code>csvFile</code> for the given <code>tablename</code> <code>addresses</code> will be used.
+	 *
+	 * @param tablename The name of the database table like in the SQL statement (e.g. addresses).
+	 * @param csvFile   A {@link File} object of a CSV file which should be parsed in order to return table data.
+	 */
+	public static void addTableResource( String tablename, File csvFile ) {
+		Map<String, File> databaseMap;
+		if( tableResources.containsKey( DEFAULT_DATABASE ) ) {
+			databaseMap = tableResources.get( DEFAULT_DATABASE );
+		} else {
+			databaseMap = Collections.synchronizedMap( new HashMap<String, File>() );
+		}
+		databaseMap.put( tablename.toLowerCase(), csvFile );
+		tableResources.put( DEFAULT_DATABASE, databaseMap );
+	}
 
-    @Override
-    public int getMajorVersion() {
-        return 1;
-    }
+	@Override
+	public int getMajorVersion() {
+		return 1;
+	}
 
-    @Override
-    public int getMinorVersion() {
-        return 0;
-    }
+	@Override
+	public int getMinorVersion() {
+		return 0;
+	}
 
-    @Override
-    public boolean jdbcCompliant() {
-        return false;
-    }
+	@Override
+	public boolean jdbcCompliant() {
+		return false;
+	}
 
-    @Override
-    public boolean acceptsURL(String url) throws SQLException {
-        return true;
-    }
+	@Override
+	public boolean acceptsURL( String url ) throws SQLException {
+		return true;
+	}
 
-    @Override
-    public Connection connect(String url, Properties info) throws SQLException {
-        String database = parseConnectUrl(url);
+	@Override
+	public Connection connect( String url, Properties info ) throws SQLException {
+		String database = parseConnectUrl( url );
 
-        loadTableResources(database);
+		loadTableResources( database );
 
-        return new DummyConnection(tableResources.get(database));
-    }
+		return new DummyConnection( tableResources.get( database ) );
+	}
 
-    @Override
-    public DriverPropertyInfo[] getPropertyInfo(final String url, final Properties props) throws SQLException {
-        return new DriverPropertyInfo[0];
-    }
+	@Override
+	public DriverPropertyInfo[] getPropertyInfo( final String url, final Properties props ) throws SQLException {
+		return new DriverPropertyInfo[0];
+	}
 
-    @Override
-    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return null;
-    }
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		return null;
+	}
 
-    /**
-     * Parse jdbc url to database file path
-     *
-     * @param url jdbc url
-     * @return database file path
-     */
-    private String parseConnectUrl(String url) {
-        if (url == null) {
-            throw new RuntimeException("You should defined jdbc url first");
-        }
+	/**
+	 * Parse jdbc url to database file path
+	 *
+	 * @param url jdbc url
+	 * @return database file path
+	 */
+	private String parseConnectUrl( String url ) {
+		if( url == null ) {
+			throw new RuntimeException( "You should defined jdbc url first" );
+		}
 
-        final int index = url.indexOf("jdbc::mock::");
-        if (index == -1) {
-            return DEFAULT_DATABASE;
-        }
+		final int index = url.indexOf( "jdbc::mock::" );
+		if( index == -1 ) {
+			return DEFAULT_DATABASE;
+		}
 
-        final String others = url.substring("jdbc::mock::".length());
-        final String[] items = others.split("::");
-        switch (items.length) {
-            case 0:
-                throw new RuntimeException("No database directory defined");
-            default:
-                return StringUtils.join(items, "/");
-        }
+		final String others = url.substring( "jdbc::mock::".length() );
+		final String[] items = others.split( "::" );
+		if( items.length == 0 ) {
+			throw new RuntimeException( "No database directory defined" );
+		}
+		return StringUtils.join( items, "/" );
+	}
 
-    }
+	/**
+	 * load table resources from database directory
+	 *
+	 * @param database database path
+	 */
+	private void loadTableResources( String database ) {
+		// ignore database name is any
+		if( DEFAULT_DATABASE.equals( database ) ) {
+			return;
+		}
 
-    /**
-     * load table resources from database directory
-     *
-     * @param database database path
-     */
-    private void loadTableResources(String database) {
-        // ignore database name is any
-        if (DEFAULT_DATABASE.equals(database)) {
-            return;
-        }
+		// check database is exists
+		URL dirUrl = getClass().getClassLoader().getResource( database );
+		if( dirUrl == null ) {
+			throw new RuntimeException( "The database directory is not exists" );
+		}
 
-        // check database is exists
-        URL dirUrl = getClass().getClassLoader().getResource(database);
-        if (dirUrl == null) {
-            throw new RuntimeException("The database directory is not exists");
-        }
+		File dir = new File( dirUrl.getFile() );
+		if( !dir.canRead() || !dir.isDirectory() ) {
+			throw new RuntimeException( "The database directory is not a directory or cannot read" );
+		}
 
-        File dir = new File(dirUrl.getFile());
-        if (!dir.canRead() || !dir.isDirectory()) {
-            throw new RuntimeException("The database directory is not a directory or cannot read");
-        }
+		// get all table files
+		File[] files = dir.listFiles( new FileFilter() {
 
-        // get all table files
-        File[] files = dir.listFiles(new FileFilter() {
+			@Override
+			public boolean accept( File pathname ) {
+				return pathname.isFile() && FilenameUtils.isExtension( pathname.getName(), "csv" );
+			}
 
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.isFile() && FilenameUtils.isExtension(pathname.getName(), "csv");
-            }
+		} );
 
-        });
-
-        // registry table resources
-        for (File file : files) {
-            Map<String, File> databaseMap = tableResources.get(database);
-            if (databaseMap == null) {
-                databaseMap = Collections.synchronizedMap(new HashMap<String, File>());
-                tableResources.put(database, databaseMap);
-            }
-            databaseMap.put(FilenameUtils.getBaseName(file.getName()), file);
-        }
-    }
-
+		// registry table resources
+		for( File file : files ) {
+			Map<String, File> databaseMap = tableResources.get( database );
+			if( databaseMap == null ) {
+				databaseMap = Collections.synchronizedMap( new HashMap<String, File>() );
+				tableResources.put( database, databaseMap );
+			}
+			databaseMap.put( FilenameUtils.getBaseName( file.getName() ), file );
+		}
+	}
 
 }
--- src/test/java/com/googlecode/dummyjdbc/MultipleTableResourcesTest.java
+++ src/test/java/com/googlecode/dummyjdbc/MultipleTableResourcesTest.java
@@ -16,59 +16,59 @@ import static org.junit.Assert.assertTrue;
 
 public class MultipleTableResourcesTest {
 
-    public static final String INVOICE_POSITIONS_TABLE = "BOO.[dbo].[invoice_positions]";
-    public static final String INVOICE_TABLE_NAME = "BOO.[dbo].[invoices]";
+	public static final String INVOICE_POSITIONS_TABLE = "BOO.[dbo].[invoice_positions]";
+	public static final String INVOICE_TABLE_NAME = "BOO.[dbo].[invoices]";
 
-    private File testFileInvoices;
-    private File testFileInvoiceAccounts;
+	private File testFileInvoices;
+	private File testFileInvoiceAccounts;
 
-    @Before
-    public void setup() {
-        testFileInvoices = new File("src/test/resources/invoices/invoices.csv");
-        testFileInvoiceAccounts = new File("src/test/resources/invoices/invoice_positions.csv");
-    }
+	@Before
+	public void setup() {
+		testFileInvoices = new File( "src/test/resources/invoices/invoices.csv" );
+		testFileInvoiceAccounts = new File( "src/test/resources/invoices/invoice_positions.csv" );
+	}
 
-    @Test
-    public void testStatementIsCsvStatement() throws Exception {
-        givenDummyDriverWithTables();
+	@Test
+	public void testStatementIsCsvStatement() throws Exception {
+		givenDummyDriverWithTables();
 
-        Connection connection = DriverManager.getConnection("any");
-        Statement stmt = connection.createStatement();
+		Connection connection = DriverManager.getConnection( "any" );
+		Statement stmt = connection.createStatement();
 
-        assertTrue(stmt instanceof CsvStatement);
-    }
+		assertTrue( stmt instanceof CsvStatement );
+	}
 
-    @Test
-    public void testCsvStatementContainsMapWithTwoTableResources() throws Exception {
-        givenDummyDriverWithTables();
+	@Test
+	public void testCsvStatementContainsMapWithTwoTableResources() throws Exception {
+		givenDummyDriverWithTables();
 
-        Connection connection = DriverManager.getConnection("any");
-        Statement stmt = connection.createStatement();
+		Connection connection = DriverManager.getConnection( "any" );
+		Statement stmt = connection.createStatement();
 
-        Field field = stmt.getClass().getDeclaredField("tableResources");
-        field.setAccessible(true);
-        Map<String, File> tableResources = (Map<String, File>) field.get(stmt);
-        assertTrue(tableResources.containsKey(INVOICE_TABLE_NAME.toLowerCase()));
-        assertTrue(tableResources.containsKey(INVOICE_POSITIONS_TABLE.toLowerCase()));
-    }
+		Field field = stmt.getClass().getDeclaredField( "tableResources" );
+		field.setAccessible( true );
+		Map<String, File> tableResources = ( Map<String, File> ) field.get( stmt );
+		assertTrue( tableResources.containsKey( INVOICE_TABLE_NAME.toLowerCase() ) );
+		assertTrue( tableResources.containsKey( INVOICE_POSITIONS_TABLE.toLowerCase() ) );
+	}
 
-    @Test
-    public void testCsvStatementForTwoIndividualQueriesYieldsResults() throws Exception {
-        givenDummyDriverWithTables();
+	@Test
+	public void testCsvStatementForTwoIndividualQueriesYieldsResults() throws Exception {
+		givenDummyDriverWithTables();
 
-        Connection connection = DriverManager.getConnection("any");
-        Statement stmt = connection.createStatement();
-        ResultSet rsOne = stmt.executeQuery("SELECT * FROM BOO.[dbo].[invoices] WHERE ID = '4711'");
-        ResultSet rsTwo = stmt.executeQuery("SELECT * FROM BOO.[dbo].[invoice_positions] WHERE ID = '4711'");
+		Connection connection = DriverManager.getConnection( "any" );
+		Statement stmt = connection.createStatement();
+		ResultSet rsOne = stmt.executeQuery( "SELECT * FROM BOO.[dbo].[invoices] WHERE ID = '4711'" );
+		ResultSet rsTwo = stmt.executeQuery( "SELECT * FROM BOO.[dbo].[invoice_positions] WHERE ID = '4711'" );
 
-        assertTrue(rsOne.next());
-        assertTrue(rsTwo.next());
-    }
+		assertTrue( rsOne.next() );
+		assertTrue( rsTwo.next() );
+	}
 
-    private void givenDummyDriverWithTables() throws ClassNotFoundException {
-        Class.forName(DummyJdbcDriver.class.getCanonicalName());
-        DummyJdbcDriver.addTableResource(INVOICE_TABLE_NAME, testFileInvoices);
-        DummyJdbcDriver.addTableResource(INVOICE_POSITIONS_TABLE, testFileInvoiceAccounts);
-    }
+	private void givenDummyDriverWithTables() throws ClassNotFoundException {
+		Class.forName( DummyJdbcDriver.class.getCanonicalName() );
+		DummyJdbcDriver.addTableResource( INVOICE_TABLE_NAME, testFileInvoices );
+		DummyJdbcDriver.addTableResource( INVOICE_POSITIONS_TABLE, testFileInvoiceAccounts );
+	}
 
 }
